c25db34d1cafc013ea0fd72e95b19bf0e3f91fcb
irene <irene.xu71@gmail.com>
2025-12-22 20:20:11 -0500

    editting defaults

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,18 @@ const Navbar = () => {
                 IX
             </p>
         </NavLink>
+        <nav className="flex text-lg gap-7 font-medium">
+            <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500'  : 'text-black'}>
+                About
+            </NavLink>
 
-        
+            <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500'  : 'text-black'}>
+                Projects
+            </NavLink>
+
+        </nav>
+
+
     </header>
   )
 }
